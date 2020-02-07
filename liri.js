4edb11222363464a0f886073f87441439bf8d522
Fung <koufongso@gmail.com>
2020-02-06 23:55:00 -0800

    added search spotify function

--- liri.js
+++ liri.js
@@ -1,11 +1,6 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 
-var Spotify = require('node-spotify-api');
-var spotify = new Spotify(keys.spotify);
-
-console.log(spotify);
-
 
 var input = process.argv.splice(2); // take out the first 2 "irrelevent input"
 var cmd = input[0]; // take first input as command
@@ -17,7 +12,6 @@ console.log(target);
 // command cases
 switch (cmd) {
     case "concert-this":
-        console.log("----> Call concert-this")
         SearchBandIntTown(target);
         break;
     case "spotify-this-song":
@@ -37,9 +31,10 @@ switch (cmd) {
 // define corresponding functions
 /**
  * It will call Bands in Town Artist Events API and display the information 
- * @param {String} name artist/band name
+ * @param {String[]} name artist/band name
  */
 function SearchBandIntTown(name) {
+    name = name.join("+");// process the name
     var axios = require("axios");
     var id = "codingbootcamp";
     var query = "https://rest.bandsintown.com/artists/" + name + "/events?app_id=" + id;
@@ -47,38 +42,38 @@ function SearchBandIntTown(name) {
         .then(function (response) {
             console.log("-----------------------------------");
             console.log(response.data.length);
-            for(var i =0 ; i<response.data.length;i++){
+            for (var i = 0; i < response.data.length; i++) {
                 var venue = response.data[i].venue;
                 // process location string
-                var location ="";
-                if(venue.city!=""){
-                    location+=venue.city;
+                var location = "";
+                if (venue.city != "") {
+                    location += venue.city;
                 }
-                if(venue.region!=""){
-                    if(location!=""){
-                        location+=", "
+                if (venue.region != "") {
+                    if (location != "") {
+                        location += ", "
                     }
-                    location+=venue.city;
+                    location += venue.city;
                 }
 
-                if(venue.country!=""){
-                    if(location!=""){
-                        location+=", "
+                if (venue.country != "") {
+                    if (location != "") {
+                        location += ", "
                     }
-                    location+=venue.country;
+                    location += venue.country;
                 }
 
                 // process date into MM/DD/YYYY format
-                var temp  = response.data[i].datetime.split("T")[0];
+                var temp = response.data[i].datetime.split("T")[0];
                 temp = temp.split("-");
-                var date = temp[1]+"/"+temp[2]+"/"+temp[0];
+                var date = temp[1] + "/" + temp[2] + "/" + temp[0];
 
 
                 console.log("-----------------------------------");
-                console.log("-----------------"+i+"-----------------");
-                console.log("Name of Venue: "+ venue.name);
-                console.log("Venue Location: "+ location);
-                console.log("Date of Evnet: "+ date);
+                console.log("-----------------" + (i + 1) + "-----------------");
+                console.log("Name of Venue: " + venue.name);
+                console.log("Venue Location: " + location);
+                console.log("Date of Evnet: " + date);
             }
         })
         .catch(function (error) {
@@ -92,5 +87,44 @@ function SearchBandIntTown(name) {
         })
 }
 
+
+
+/**
+ * It will call Spotify API to search the song and display the info
+ * @param {String[]} name search song name in Spotify 
+ */
+function SearchSpotify(name) {
+    name = name.join(" "); // process name
+    var Spotify = require('node-spotify-api');
+    var spotify = new Spotify(keys.spotify);
+    spotify
+        .search({ type: 'track', query: name })
+        .then(function (response) {
+            var data = response.tracks.items[0];
+            // console.log(data);
+            var artists = "";
+            for(var i=0; i<data.artists.length;i++){
+                artists+=data.artists[i].name;
+                if(i!=data.artists.length-1){
+                    artists+=", ";
+                }
+            }
+            console.log("-----------------------------------");
+            console.log("Artist(s): "+artists);
+            console.log("Song: "+ data.name);
+            console.log("Preview link: "+data.preview_url);
+            console.log("Album: "+ data.album.name);
+
+        })
+        .catch(function (err) {
+            if (err.response) {
+                console.log(err.response.data);
+            } else if (err.request) {
+                console.log(err.request);
+            } else {
+                console.log("Error", err.message);
+            }
+        });
+}
 
 
